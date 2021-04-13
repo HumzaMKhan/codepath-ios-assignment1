a578b60a783c7f61571f26f6c6648517ebf2b58d
HumzaMKhan <humzamk@uci.edu>
2021-04-12 23:15:11 -0700

    Minimum requirements for assignment 2 met.

---
+++ flixter/flixter/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by Humza Khan on 4/12/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+}
